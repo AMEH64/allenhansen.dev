a77dbe891bb6dc8ed31dac8fe585dd019f50e29e
Allen Hansen <allen.hansen64@gmail.com>
2020-06-06 12:16:57 -0500

    Clean up code and add animations
    Minimized CSS and JS files.
    Added AOS animation library.
    Improved responsiveness.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,154 +1,92 @@
-// (function($) {
-//   "use strict"; // Start of use strict
+(function($) {
+  "use strict"; // Start of use strict
 
-//   // Smooth scrolling using jQuery easing
-//   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: (target.offset().top - 48)
-//         }, 1000, "easeInOutExpo");
-//         return false;
-//       }
-//     }
-//   });
+  // Debugging mysterious elements with x-axis overflow
+  // $(document).ready(function() {   
+  //     var docWidth = document.documentElement.offsetWidth;
 
-//   // Closes responsive menu when a scroll trigger link is clicked
-//   $('.js-scroll-trigger').click(function() {
-//     $('.navbar-collapse').collapse('hide');
-//   });
+  //     [].forEach.call(
+  //         document.querySelectorAll('*'),
+  //         function(el) {
+  //             if (el.offsetWidth > docWidth) {
+  //                 console.log(el);
+  //             }
+  //         }
+  //     );
+  // });
 
-//   // Activate scrollspy to add active class to navbar items on scroll
-//   $('body').scrollspy({
-//     target: '#mainNav',
-//     offset: 54
-//   });
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: "#navbar", offset: 50}); 
 
-//   // Collapse Navbar
-//   var navbarCollapse = function() {
-//     if ($("#mainNav").offset().top > 100) {
-//       $("#mainNav").addClass("navbar-shrink");
-//     } else {
-//       $("#mainNav").removeClass("navbar-shrink");
-//     }
-//   };
-//   // Collapse now if page is not at top
-//   navbarCollapse();
-//   // Collapse the navbar when page is scrolled
-//   $(window).scroll(navbarCollapse);
+  // Add smooth scrolling on all links inside the navbar
+  $("#navbar a").click(function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+          // Store hash
+          var hash = this.hash;
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 800, function() {  
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+          });
+          $("#navbar").find(".active").removeClass("active");
+          $(this).addClass("active");
+      }  // End if
+  });
 
-//   var hasShown = false;
-//   $(window).scroll(function() {
-//     function elementScrolled(elem) {
-//       var docViewTop = $(window).scrollTop();
-//       var docViewBottom = docViewTop + $(window).height();
-//       var elemTop = $(elem).offset().top + 300;
-//       return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
-//     }
-
-//    if (elementScrolled("#skills") && !hasShown) { 
-//       var delay = 500;
-//       $(".progress-bar").each(function(i) {
-//         $(this)//.delay(delay)
-//         .animate({
-//           width: $(this).attr('aria-valuenow') + '%'
-//         }//, {duration: delay * 3}
-//         , delay);
-      
-//         $(this).prop('Counter', 0).animate({
-//           Counter: $(this).text()
-//         }, {
-//           duration: delay * 3,
-//           // easing: 'swing',
-//           step: function(now) {
-//             $(this).text(Math.ceil(now) + '%');
-//           }
-//         });
-//       });
-//       hasShown = true;
-//     }
-//   });
-// })(jQuery); // End of use strict
-
-// // Theme-Selector
-// $("#theme-toggle").click(function() {
-//   if ($("body").hasClass("bg-light")) {
-//       $("body").addClass("bg-dark").removeClass("bg-light"); 
-//       $("#mainNav").addClass("bg-dark").removeClass("bg-light");
-//       $("#mainNav").addClass("navbar-dark").removeClass("navbar-light");      
-//       $("button:not(.navbar-toggler)").addClass("btn-outline-light").removeClass("btn-outline-dark"); 
-//       $("#linkedIn-light").hide();
-//       $("#linkedIn-dark").show();
-//   }
-//   else {
-//       $("body").addClass("bg-light").removeClass("bg-dark"); 
-//       $("#mainNav").addClass("bg-light").removeClass("bg-dark");
-//       $("#mainNav").addClass("navbar-light").removeClass("navbar-dark");   
-//       $("button:not(.navbar-toggler)").addClass("btn-outline-dark").removeClass("btn-outline-light");
-//       $("#linkedIn-dark").hide();
-//       $("#linkedIn-light").show();
-//   }   
-// });   
-
-// BEGIN: Typer
-var TxtType = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-
-  if (this.isDeleting) { 
-    delta /= 2; 
-  }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('typewrite');
-  for (var i = 0; i < elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
+  $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+          $('#navbar').addClass('shadow-lg');
+      } else {
+          $('#navbar').removeClass('shadow-lg');
       }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = " ";
-  document.body.appendChild(css);
-};
-// END: Typer
+  });
+
+  // Animations
+  AOS.init({
+    duration: 2000
+  });
+
+  // Contact Form
+  $('#openInMailApp').click(function(event) {    
+      var form = $('form');
+
+      if (form[0].checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+      }
+      else {
+          var name = $('#name').val();
+          var pronouns = $('#pronouns').val();
+          var subject = $('#subject').val();
+          var url = "mailto:allen.hansen64@gmail.com" 
+              + "?subject=[AMEH - " + (subject === "" ? "Inquiry" : subject) + "]"
+              + "&body=Hello,%0A%0A%0A%0ASincerely, %0A%0A"
+              + name
+              + "%0A"
+              + pronouns;
+
+          window.open(url, '_blank');
+      }
+
+      form.addClass('was-validated');
+  });
+
+  // Carousel settings
+  // $('.carousel').carousel({
+  //     interval: 2000
+  // });
+
+  // Toasts
+  // $('.toast').toast('show');
+
+  // Tooltips
+  // $("body").tooltip({ 
+  //     selector: '[data-toggle=tooltip]' 
+  // });
+})(jQuery); // End of use strict
