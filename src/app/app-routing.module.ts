b62ede49d07eb9baa31cb39d146cb861930d5af5
Allen Hansen <allen.hansen64@gmail.com>
2022-08-06 20:59:17 -0400

    initial tailwindcss hero section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ErrorComponent, PageNotFoundComponent } from '@shared/components';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
+import { ErrorComponent, PageNotFoundComponent } from './components';
+
+@NgModule({
+  declarations: [ErrorComponent, PageNotFoundComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [CommonModule, FontAwesomeModule],
+})
+export class SharedModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    library.addIcons(faArrowDown, faGithub, faLinkedin, faTwitter);
+  }
+}
