ce0ae4fdd2758a1b29387f79bfd61c5b755774fd
Allen Hansen <allen.hansen64@gmail.com>
2022-08-29 14:07:17 -0400

    #56 migrated away from @apply

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'ameh-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  host: {
+    class: 'block container mx-auto px-8 md:px-14 lg:px-16',
+  },
 })
 export class HomeComponent implements OnInit {
   constructor() {}
--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ameh-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
+  host: {
+    class: 'flex flex-col justify-center items-center h-screen w-screen gap-3',
+  },
 })
 export class PageNotFoundComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
