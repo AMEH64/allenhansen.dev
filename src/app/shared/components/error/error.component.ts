845adfe221ba270fab783befa3785b1ad85014c5
Allen Hansen <allen.hansen64@gmail.com>
2022-08-29 14:07:17 -0400

    #56 migrated away from @apply

--- src/app/shared/components/error/error.component.ts
+++ src/app/shared/components/error/error.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ameh-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  styleUrls: ['./error.component.scss'],
+  host: {
+    class: 'flex flex-col justify-center items-center h-screen w-screen gap-3',
+  },
 })
 export class ErrorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
