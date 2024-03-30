dd940150668184074e7a3e92a46c28538a83acbf
Allen Hansen <allen.hansen64@gmail.com>
2024-03-29 23:09:54 -0400

    Remove defaults from prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,20 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   arrowParens: 'avoid',
-  embeddedLanguageFormatting: 'auto',
-  endOfLine: 'lf',
-  htmlWhitespaceSensitivity: 'css',
-  insertPragma: false,
-  bracketSameLine: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
   proseWrap: 'always',
-  quoteProps: 'as-needed',
   semi: false,
   singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-  useTabs: false,
   plugins: ['prettier-plugin-tailwindcss'],
   tailwindFunctions: ['classNames'],
 }
