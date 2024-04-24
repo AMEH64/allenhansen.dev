c50b980b21da9cc72af0b80a404fdc5f0cd28ad6
Allen Hansen <allen.hansen64@gmail.com>
2024-04-24 01:30:55 -0400

    Simplify tailwind.config.ts file
    Removed the extension of font families in the tailwind configuration file. Now, the file simply lists content directories and does not include any custom theme settings. This provides a cleaner, simpler configuration setup.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,9 @@
 import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        mono: ['GeistMono', ...defaultTheme.fontFamily.mono],
-        sans: ['GeistSans', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config
