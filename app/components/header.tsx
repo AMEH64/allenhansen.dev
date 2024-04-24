eefab39d9d8e089903e23568743fa35f89f1b8f9
Allen Hansen <allen.hansen64@gmail.com>
2024-04-24 01:31:43 -0400

    Update header styling and homepage text
    The styling for the header button has been updated to include tighter tracking. The homepage introductory text has been enhanced for better readability and emphasis on key elements. An SVG path with CSS effects was added to create emphasis within the text.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,7 +6,7 @@ export const Header = () => (
   <header className="sticky top-0 z-10 w-full border-b border-zinc-300/40 bg-zinc-100 backdrop-blur supports-[backdrop-filter]:bg-zinc-100/50 dark:border-zinc-700/40 dark:bg-zinc-900 dark:supports-[backdrop-filter]:bg-zinc-900/50">
     <div className="flex h-14 max-w-screen-2xl items-center justify-between gap-4 px-4 sm:px-6 lg:px-8">
       <div>
-        <Button to="/" className="font-mono italic" plain>
+        <Button to="/" className="font-mono italic tracking-tight" plain>
           &lt;AllenHansen&nbsp;/&gt;
         </Button>
       </div>
