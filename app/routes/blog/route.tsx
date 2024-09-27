d41cd64be95b2ba56948bfb8fa12a06df62fdb59
Allen Hansen <allen.hansen64@gmail.com>
2024-09-27 15:56:27 -0400

    Refactor: Replace Header and Footer with Layout
    This change consolidates the layout structure by using a single `Layout` component instead of separate `Header` and `Footer` components in the `Index` route. This simplifies the JSX structure and centralizes layout management, making the code more maintainable.

--- app/routes/blog/route.tsx
+++ app/routes/blog/route.tsx
@@ -1,26 +1,38 @@
-import { Outlet } from '@remix-run/react'
-import { Header } from '~/components/header'
-import React, { PropsWithChildren } from 'react'
-import { Footer } from '~/components/footer'
-
-const Layout = ({ children }: PropsWithChildren) => (
-  <div className="flex min-h-svh flex-col">
-    <Header />
-    <main className="mx-auto flex w-full max-w-screen-xl flex-1 bg-white px-4 sm:px-6 lg:px-8 dark:bg-zinc-900">
-      <div className="lg:y-8 mx-auto flex max-w-screen-md flex-1 py-4 sm:py-6">
-        {children}
-      </div>
-    </main>
-    <Footer />
-  </div>
-)
+import { Outlet, useMatches } from '@remix-run/react'
+import React from 'react'
+import { Layout } from '~/components/layout'
+import { intlFormat } from 'date-fns/intlFormat'
+import { postMetaSchema } from '~/types/blog/post-meta'
 
 export default function Route() {
+  const matches = useMatches()
+
+  const postMeta = postMetaSchema.parse(
+    matches
+      .filter(m => m.handle)
+      .map(m => m.handle)
+      .at(0),
+  )
+
   return (
     <Layout>
-      <article className="prose dark:prose-invert">
-        <h1>Hello world!</h1>
-        <Outlet />
+      <article>
+        <header className="flex flex-col">
+          <h1 className="mt-6 text-balance text-4xl font-bold tracking-tight sm:text-5xl">
+            {postMeta.title}
+          </h1>
+          <p className="mt-3">{postMeta.description}</p>
+          <time
+            dateTime={postMeta.publishedOn}
+            className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+          >
+            <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+            <span className="ml-3">{intlFormat(postMeta.publishedOn)}</span>
+          </time>
+        </header>
+        <div className="prose mt-8 dark:prose-invert">
+          <Outlet />
+        </div>
       </article>
     </Layout>
   )
