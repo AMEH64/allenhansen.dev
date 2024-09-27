3a5ecf2e137a9b91ec807c6c2ec6ec0b1780c392
Allen Hansen <allen.hansen64@gmail.com>
2024-09-27 00:24:50 -0400

    Refactor layout and delete outdated post
    Refactor the application layout to use a unified Layout component for consistent structure and styling across pages. Removed the outdated "Hello World" post from the content directory. Updated the Header and Footer components for better UI consistency and responsiveness across different screen sizes.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,21 +3,24 @@ import { Button } from '~/components/ui/button'
 import { GitHubIcon } from '~/components/icons/git-hub-icon'
 
 export const Header = () => (
-  <header className="sticky top-0 z-10 w-full border-b border-zinc-300/40 bg-zinc-100 backdrop-blur supports-[backdrop-filter]:bg-zinc-100/50 dark:border-zinc-700/40 dark:bg-zinc-900 dark:supports-[backdrop-filter]:bg-zinc-900/50">
-    <div className="flex h-14 max-w-screen-2xl items-center justify-between gap-4 px-4 sm:px-6 lg:px-8">
-      <div>
+  <header className="sticky top-0 z-10 w-full border-b border-zinc-300/40 bg-white shadow backdrop-blur supports-[backdrop-filter]:bg-white/65 dark:border-zinc-700/40 dark:bg-zinc-900 dark:shadow-zinc-950 dark:supports-[backdrop-filter]:bg-zinc-900/65">
+    <div className="mx-auto flex h-16 max-w-screen-2xl flex-1 items-center justify-between gap-4 px-4 sm:px-6 lg:px-8">
+      <div className="flex items-center justify-start gap-3">
         <Button to="/" className="font-mono italic tracking-tight" plain>
           &lt;AllenHansen&nbsp;/&gt;
         </Button>
       </div>
-      <div className="flex flex-1 items-center justify-end gap-3">
+      <div className="flex items-center justify-end gap-3">
         <Button
           to="https://github.com/AMEH64"
           target="_blank"
           rel="noreferrer"
           plain
         >
-          <GitHubIcon title="Allen's GitHub Profile" />
+          <GitHubIcon
+            className="[--btn-icon:theme(colors.zinc.950)] dark:[--btn-icon:theme(colors.zinc.50)]"
+            title="Allen's GitHub Profile"
+          />
         </Button>
         <Button
           to="https://x.com/ameh64"
@@ -25,7 +28,10 @@ export const Header = () => (
           rel="noreferrer"
           plain
         >
-          <XIcon title="Allen's Twitter Profile" />
+          <XIcon
+            className="[--btn-icon:theme(colors.zinc.950)] dark:[--btn-icon:theme(colors.zinc.50)]"
+            title="Allen's Twitter Profile"
+          />
         </Button>
       </div>
     </div>
--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,9 +1,8 @@
-import { Outlet } from '@remix-run/react'
-import { Header } from '~/components/header'
 import React, { PropsWithChildren } from 'react'
+import { Header } from '~/components/header'
 import { Footer } from '~/components/footer'
 
-const Layout = ({ children }: PropsWithChildren) => (
+export const Layout = ({ children }: PropsWithChildren) => (
   <div className="flex min-h-svh flex-col">
     <Header />
     <main className="mx-auto flex w-full max-w-screen-xl flex-1 bg-white px-4 sm:px-6 lg:px-8 dark:bg-zinc-900">
@@ -14,14 +13,3 @@ const Layout = ({ children }: PropsWithChildren) => (
     <Footer />
   </div>
 )
-
-export default function Route() {
-  return (
-    <Layout>
-      <article className="prose dark:prose-invert">
-        <h1>Hello world!</h1>
-        <Outlet />
-      </article>
-    </Layout>
-  )
-}
