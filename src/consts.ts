f741d4b785bbd9aa16e3be76588e11a112892131
Allen Hansen <allen.hansen64@gmail.com>
2024-10-03 22:34:51 -0400

    Add constants for title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = 'Allen Hansen'
+export const SITE_DESCRIPTION = 'Welcome to my portfolio!'
