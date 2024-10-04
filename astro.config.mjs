8a9f76c22d78eb06b10e939a3effdc3d7e354479
Allen Hansen <allen.hansen64@gmail.com>
2024-10-04 00:55:21 -0400

    Migrate to Astro (#101)
    * Migrate to Astro
    
    * Add constants for title and description
    
    * Remove unused imports from main layout
    
    * Clean up date formatting
    
    * Update blog index
    
    * Update blog layout

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://allenhansen.dev',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'hybrid',
+  adapter: vercel({ webAnalytics: { enabled: true } }),
+})
---
+++ src/content/config.ts
@@ -0,0 +1,48 @@
+import { defineCollection, z } from 'astro:content'
+
+const blog = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.string().date(),
+      updatedDate: z.string().date().optional(),
+      image: image().refine(img => img.width >= 1080, {
+        message: 'Cover image must be at least 1080 pixels wide!',
+      }),
+    }),
+})
+
+const jobsCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    position: z.string().min(1),
+    company: z.string().min(1),
+    location: z.string().min(1),
+    startDate: z.string().date(),
+    endDate: z.string().date().optional(),
+    summary: z.string().min(1),
+    highlights: z.string().min(1).array().min(3).max(7),
+    skills: z.string().min(1).array().min(1).optional(),
+  }),
+})
+
+const educationCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    degree: z.string().min(1),
+    school: z.string().min(1),
+    location: z.string().min(1),
+    graduationDate: z.string().date(),
+    summary: z.string().min(1),
+  }),
+})
+
+export const collections = {
+  blog,
+  jobs: jobsCollection,
+  education: educationCollection,
+}
