899d21880c1e011f5b48d72315a27226ed74a54d
Allen Hansen <allen.hansen64@gmail.com>
2024-12-13 01:03:09 -0500

    Add color mode toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ import typography from '@tailwindcss/typography'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ['selector', '[data-color-mode="dark"]'],
   theme: {
     extend: {
       supports: {
