72085f67cbf81fd798cf590910005b8fc3e4511c
Allen Hansen <allen.hansen64@gmail.com>
2025-01-05 15:14:01 -0500

    fix: change output mode from hybrid to static in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://allenhansen.dev',
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel({ webAnalytics: { enabled: true } }),
 })
