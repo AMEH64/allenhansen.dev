800065096459fea52668438894ee3bed64fbfbc2
Allen Hansen <me@allenhansen.dev>
2025-03-26 22:22:06 -0400

    chore: clean up Tailwind CSS configuration
    - Comment out unused plugin configurations in tailwind.config.mjs
    - Maintain structure for future plugin additions while improving readability

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,31 +6,31 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-    //   supports: {
-    //     'field-sizing-content': 'field-sizing: content',
-    //     'resize-y': 'resize: vertical',
-    //   },
+      //   supports: {
+      //     'field-sizing-content': 'field-sizing: content',
+      //     'resize-y': 'resize: vertical',
+      //   },
     },
   },
-//   plugins: [
-//     plugin(({ addVariant, addUtilities, matchUtilities }) => {
-//       addVariant('popover-open', '&:popover-open')
-//       // https://github.com/tailwindlabs/tailwindcss/pull/13329
-//       addVariant('starting', '@starting-style')
-//       // https://github.com/tailwindlabs/tailwindcss/pull/12149
-//       addUtilities({
-//         '.transition-allow-discrete': {
-//           'transition-behavior': 'allow-discrete',
-//         },
-//         '.field-sizing-content': {
-//           'field-sizing': 'content',
-//         },
-//       })
-//       matchUtilities({
-//         'block-size': value => ({ blockSize: value }),
-//         'max-block-size': value => ({ maxBlockSize: value }),
-//       })
-//     }),
+  plugins: [
+    //     plugin(({ addVariant, addUtilities, matchUtilities }) => {
+    //       addVariant('popover-open', '&:popover-open')
+    //       // https://github.com/tailwindlabs/tailwindcss/pull/13329
+    //       addVariant('starting', '@starting-style')
+    //       // https://github.com/tailwindlabs/tailwindcss/pull/12149
+    //       addUtilities({
+    //         '.transition-allow-discrete': {
+    //           'transition-behavior': 'allow-discrete',
+    //         },
+    //         '.field-sizing-content': {
+    //           'field-sizing': 'content',
+    //         },
+    //       })
+    //       matchUtilities({
+    //         'block-size': value => ({ blockSize: value }),
+    //         'max-block-size': value => ({ maxBlockSize: value }),
+    //       })
+    //     }),
     typography,
   ],
 }
